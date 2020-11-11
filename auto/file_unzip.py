5afb8ace24a59c72850d17a3f32a852cac4697d6
HayeonBaek <bhy0512@gmail.com>
2020-11-11 13:36:55 +0900

    TIL: python decompress zip files and txt file read to xlsx

--- auto/file_unzip.py
+++ auto/file_unzip.py
@@ -1,7 +1,7 @@
 import os
 import glob
 from zipfile import ZipFile
-import openpyxl
+from openpyxl import load_workbook, Workbook
 
 def unzip_file():
     txt_file_list = glob.glob('./auto/txt/*')
@@ -15,7 +15,7 @@ def unzip_file():
         folder_name = os.path.basename(file).split('.')[0]
         # print(folder_name)
 
-        with ZipFile(file, 'r') as zip:
+        with ZipFile(file, 'r',) as zip:
             zip.printdir()
 
             print(f'Extracting all the files now... {file}')
@@ -23,6 +23,7 @@ def unzip_file():
             print('TXT DONE!')
 
     # xml 파일 압축 해제
+    '''
     for file in xml_file_list:
         folder_name = os.path.basename(file).split('.')[0]
         # print(folder_name)
@@ -33,8 +34,9 @@ def unzip_file():
             print(f'Extracting all the files now... {file}')
             zip.extractall(f'./auto/temp/xml/{folder_name}')
             print('XML DONE!')
+        '''
 
 if __name__ == '__main__':
-    print('main.py 실행 중...')
+    print('실행 중...')
     unzip_file()
-    print('DONE')
+    print('ALL DONE!!!!!')
---
+++ auto/txt_file_read.py
@@ -0,0 +1,27 @@
+import os
+from openpyxl import load_workbook, Workbook
+
+xlsx = Workbook()
+sheet = xlsx.active    
+sheet.freeze_panes = 'A2'
+
+sheet.append(['폴더명','txt_파일명', '0(dish)', 'xmin', 'ymin', 'xmax', 'ymax','1(food)', 'xmin', 'ymin', 'xmax', 'ymax'])     
+
+txt_contents = []
+for root, dirs, files in os.walk('./auto/temp/txt'):
+    for fname in files:
+        file_path = os.path.join(root, fname)
+        with open(file_path, 'r', encoding='utf-8') as f:
+            contents = f.readlines()
+            folder_name = os.path.split(os.path.dirname(file_path))[1] 
+            first_field = contents[0].strip().split(' ')
+            second_field = contents[1].strip().split(' ')
+            # print(first_field, second_field)
+            # print(folder_name)
+
+            sheet.append([folder_name, fname, first_field[0], first_field[1], first_field[2], first_field[3], first_field[4], second_field[0], second_field[1], second_field[2], second_field[3], second_field[4]])
+            # print('{0} {1} {2}'.format(fname, contents[0], contents[1]))
+        f.close()
+
+xlsx.save('./auto/check_txt.xlsx')
+xlsx.close()
